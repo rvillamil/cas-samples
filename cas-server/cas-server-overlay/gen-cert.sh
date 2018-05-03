a882aa7e9f80642eb9d6b1b2fcc9f3599f442904
Rodrigo Villamil <rodrigo.villamil@diagroup.com>
2018-05-03 15:21:01 +0200

    Generador de certificados en localhost

---
+++ cas-server/cas-server-overlay/gen-cert.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+#
+# Copyright (C) Rodrigo Villamil Perez 2018
+# Fichero: gen-cert.sh
+# Fecha: 03/05/18
+#
+
+echo "Deleting previous alias and current keystore .."
+sudo rm -fv etc/cas/thekeystore
+sudo rm -fv etc/cas/thekeystore.crt
+sudo keytool -delete -alias thekeystore -keystore /etc/ssl/certs/java/cacerts -storepass changeit
+
+#DNAME="${DNAME:-CN=localhost,OU=localhost,O=localhost,L,=Madrid,ST=Madrid,C=ES}"
+DNAME="${DNAME:-CN=localhost,OU=localhost,OU=localhost,C=ES}"
+echo "Generating keystore for CAS with DN ${DNAME}"
+keytool -genkey -keyalg RSA -alias thekeystore -keystore etc/cas/thekeystore -storepass changeit -validity 360 -keysize 2048 -dname ${DNAME}
+keytool -export -alias thekeystore -file etc/cas/thekeystore.crt -keystore etc/cas/thekeystore
+sudo keytool -import -alias thekeystore -storepass changeit -file etc/cas/thekeystore.crt  -keystore /etc/ssl/certs/java/cacerts
+
+echo "Deploy keys in /etc/cas.."
+./build.sh copy
+
+
+  
