d725bad07a373339182d5a20d96737a059fd0842
Rodrigo Villamil <rodrigo.villamil@diagroup.com>
2018-05-03 16:37:07 +0200

    The path in osx and linux is different

--- cas-server/cas-server-overlay/gen-cert.sh
+++ cas-server/cas-server-overlay/gen-cert.sh
@@ -5,17 +5,22 @@
 # Fecha: 03/05/18
 #
 
+path_cacerts="/opt/java/jdk1.8.0_40_x86_64/jre/lib/security/cacerts"
+path_cacerts_osx="/etc/ssl/certs/java/cacerts"
+
+current_patch_certs=${path_cacerts}
+
 echo "Deleting previous alias and current keystore .."
 sudo rm -fv etc/cas/thekeystore
 sudo rm -fv etc/cas/thekeystore.crt
-sudo keytool -delete -alias thekeystore -keystore /etc/ssl/certs/java/cacerts -storepass changeit
+sudo keytool -delete -alias thekeystore -keystore ${current_patch_certs} -storepass changeit
 
 #DNAME="${DNAME:-CN=localhost,OU=localhost,O=localhost,L,=Madrid,ST=Madrid,C=ES}"
 DNAME="${DNAME:-CN=localhost,OU=localhost,O=localhost,C=ES}"
 echo "Generating keystore for CAS with DN ${DNAME}"
 keytool -genkey -keyalg RSA -alias thekeystore -keystore etc/cas/thekeystore -storepass changeit -validity 360 -keysize 2048 -dname ${DNAME}
 keytool -export -alias thekeystore -file etc/cas/thekeystore.crt -keystore etc/cas/thekeystore
-sudo keytool -import -alias thekeystore -storepass changeit -file etc/cas/thekeystore.crt  -keystore /etc/ssl/certs/java/cacerts
+sudo keytool -import -alias thekeystore -storepass changeit -file etc/cas/thekeystore.crt  -keystore ${current_patch_certs}
 
 echo "Deploy keys in /etc/cas.."
 ./build.sh copy
